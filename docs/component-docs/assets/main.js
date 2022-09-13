4aefe1478618bd715070e1c390dd50e93b90bb2b
Guilio Karadanais <guilio@simply.co.za>
2022-09-13 20:18:24 +0200

    Side bar link highlight

--- docs/component-docs/assets/main.js
+++ docs/component-docs/assets/main.js
@@ -1,19 +1,24 @@
-function manageSideBarLinksActiveClass(){
+function manageSideBarLinksActiveClass(currentLinkHash){
   var sideBarLinks = document.getElementsByClassName('side-bar-element-link');
 
   for (var i = 0; i < sideBarLinks.length; i++) {
+    var link = sideBarLinks[i];
 
-    sideBarLinks[i].addEventListener('click', function() {
+    if(currentLinkHash && link.href.split("#")[1] == currentLinkHash){
+      link.className += ' active';
+    }
+
+    link.addEventListener('click', function() {
+
       var current = document.getElementsByClassName('active');
       if (current.length > 0) {
         current[0].className = current[0].className.replace(' active', '');
       }
       this.className += ' active';
       });
-
   }
 }
 
 window.addEventListener('load', (event) => {
-  manageSideBarLinksActiveClass();
+  manageSideBarLinksActiveClass(window.location.hash.split('#')[1]);
 });
